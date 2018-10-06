ec3bdc82dfe938a451ceaa454ceb918d7ff38fa1
Daniel Micay <danielmicay@gmail.com>
2018-10-05 20:18:23 -0400

    BKL-L09 has the same fingerprint as BKL-L04

--- src/main/java/app/attestation/server/AttestationProtocol.java
+++ src/main/java/app/attestation/server/AttestationProtocol.java
@@ -160,7 +160,7 @@ class AttestationProtocol {
     private static final int OS_VERSION_MINIMUM = 80000;
     private static final int OS_PATCH_LEVEL_MINIMUM = 201801;
 
-    private static final String DEVICE_BKL_L04 = "Huawei Honor View 10 (BKL-L04)";
+    private static final String DEVICE_BKL_L04 = "Huawei Honor View 10";
     private static final String DEVICE_NOKIA = "Nokia (6.1, 7 Plus)";
     private static final String DEVICE_PIXEL_2 = "Google Pixel 2";
     private static final String DEVICE_PIXEL_2_XL = "Google Pixel 2 XL";
