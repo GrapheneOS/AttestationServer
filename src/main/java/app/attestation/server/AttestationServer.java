56ad3f990b1ad547a0d9771d83f6276b1af1aefd
Daniel Micay <danielmicay@gmail.com>
2021-03-26 21:13:09 -0400

    staging

--- src/main/java/app/attestation/server/AttestationServer.java
+++ src/main/java/app/attestation/server/AttestationServer.java
@@ -82,7 +82,7 @@ public class AttestationServer {
     private static final int HISTORY_PER_PAGE = 20;
     private static final long MMAP_SIZE = 1024 * 1024 * 1024;
 
-    static final String DOMAIN = "attestation.app";
+    static final String DOMAIN = "staging.attestation.app";
     private static final String ORIGIN = "https://" + DOMAIN;
 
     private static final Logger logger = Logger.getLogger(AttestationServer.class.getName());
