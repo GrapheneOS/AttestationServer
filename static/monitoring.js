569ae05b812ebac33eaa1c3d2f70923709721cd7
Daniel Micay <danielmicay@gmail.com>
2022-02-22 15:46:50 -0500

    use monospace for pairing identity field

--- static/monitoring.js
+++ static/monitoring.js
@@ -199,7 +199,9 @@ function fetchDevices() {
             };
 
             info.appendChild(create("h3", "Hardware verified information:"));
-            appendLine(info, "Pairing identity (hash of pinned hardware-backed key): " + fingerprint);
+            info.appendChild(document.createTextNode("Pairing identity (hash of pinned hardware-backed key): "));
+            info.appendChild(create("span", fingerprint, "fingerprint"));
+            info.appendChild(document.createElement("br"));
             appendLine(info, "Pinned security level: " + toSecurityLevelString(device.pinnedSecurityLevel));
             appendLine(info, "Pinned device: " + device.name);
             appendLine(info, "Pinned OS: " + device.osName);
