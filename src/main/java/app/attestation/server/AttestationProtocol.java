b6db812ae99df441b067d293e708b1739377e144
Daniel Micay <danielmicay@gmail.com>
2022-02-22 09:03:43 -0500

    port decodeChain/encodeChain from Auditor
    decodeChain replaces the existing code and decodeChain/encodeChain will
    be used to replace the database's certificate storage to support more
    than 4 certificates in the chain along with making the storage much more
    efficient as an initial step towards reducing the database size.

--- src/main/java/app/attestation/server/AttestationProtocol.java
+++ src/main/java/app/attestation/server/AttestationProtocol.java
@@ -16,6 +16,7 @@ import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,6 +41,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
 import java.util.zip.Inflater;
 
 class AttestationProtocol {
@@ -1262,25 +1265,12 @@ class AttestationProtocol {
         }
     }
 
-    static void verifySerialized(final byte[] attestationResult,
-            final Cache<ByteBuffer, Boolean> pendingChallenges, final long userId, final boolean paired)
-            throws DataFormatException, GeneralSecurityException, IOException {
-        final ByteBuffer deserializer = ByteBuffer.wrap(attestationResult);
-        final byte version = deserializer.get();
-        if (version > PROTOCOL_VERSION) {
-            throw new GeneralSecurityException("invalid protocol version: " + version);
-        } else if (version < PROTOCOL_VERSION_MINIMUM) {
-            throw new GeneralSecurityException("Auditee protocol version too old: " + version);
-        }
-
-        final short compressedChainLength = deserializer.getShort();
-        final byte[] compressedChain = new byte[compressedChainLength];
-        deserializer.get(compressedChain);
-
+    private static Certificate[] decodeChain(final byte[] dictionary, final byte[] compressedChain)
+            throws DataFormatException, GeneralSecurityException {
         final byte[] chain = new byte[MAX_ENCODED_CHAIN_LENGTH];
         final Inflater inflater = new Inflater(true);
         inflater.setInput(compressedChain);
-        inflater.setDictionary(DEFLATE_DICTIONARY_2);
+        inflater.setDictionary(dictionary);
         final int chainLength = inflater.inflate(chain);
         if (!inflater.finished()) {
             throw new GeneralSecurityException("certificate chain is too large");
@@ -1296,7 +1286,56 @@ class AttestationProtocol {
             chainDeserializer.get(encoded);
             certs.add(generateCertificate(new ByteArrayInputStream(encoded)));
         }
-        final Certificate[] certificates = certs.toArray(Certificate[]::new);
+        return certs.toArray(new Certificate[0]);
+    }
+
+    private static byte[] encodeChain(final byte[] dictionary, final Certificate[] certificates)
+            throws CertificateEncodingException, IOException {
+        final ByteBuffer chainSerializer = ByteBuffer.allocate(MAX_ENCODED_CHAIN_LENGTH);
+        for (int i = 0; i < certificates.length; i++) {
+            final byte[] encoded = certificates[i].getEncoded();
+            if (encoded.length > Short.MAX_VALUE) {
+                throw new RuntimeException("encoded certificate too long");
+            }
+            chainSerializer.putShort((short) encoded.length);
+            chainSerializer.put(encoded);
+        }
+        chainSerializer.flip();
+        final byte[] chain = new byte[chainSerializer.remaining()];
+        chainSerializer.get(chain);
+
+        if (chain.length > MAX_ENCODED_CHAIN_LENGTH) {
+            throw new RuntimeException("encoded certificate chain too long");
+        }
+
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        final Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION, true);
+        deflater.setDictionary(dictionary);
+        final DeflaterOutputStream deflaterStream = new DeflaterOutputStream(byteStream, deflater);
+        deflaterStream.write(chain);
+        deflaterStream.finish();
+        final byte[] compressed = byteStream.toByteArray();
+        //Log.d(TAG, "encoded length: " + chain.length + ", compressed length: " + compressed.length);
+
+        return compressed;
+    }
+
+    static void verifySerialized(final byte[] attestationResult,
+            final Cache<ByteBuffer, Boolean> pendingChallenges, final long userId, final boolean paired)
+            throws DataFormatException, GeneralSecurityException, IOException {
+        final ByteBuffer deserializer = ByteBuffer.wrap(attestationResult);
+        final byte version = deserializer.get();
+        if (version > PROTOCOL_VERSION) {
+            throw new GeneralSecurityException("invalid protocol version: " + version);
+        } else if (version < PROTOCOL_VERSION_MINIMUM) {
+            throw new GeneralSecurityException("Auditee protocol version too old: " + version);
+        }
+
+        final short compressedChainLength = deserializer.getShort();
+        final byte[] compressedChain = new byte[compressedChainLength];
+        deserializer.get(compressedChain);
+
+        final Certificate[] certificates = decodeChain(DEFLATE_DICTIONARY_2, compressedChain);
 
         final byte[] fingerprint = new byte[FINGERPRINT_LENGTH];
         deserializer.get(fingerprint);
