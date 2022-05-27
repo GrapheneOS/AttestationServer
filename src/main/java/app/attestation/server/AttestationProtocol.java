206da366c6784953967f1f0653f57619cf228116
Daniel Micay <danielmicay@gmail.com>
2022-05-27 17:46:51 -0400

    add key purpose check for initial key

--- src/main/java/app/attestation/server/AttestationProtocol.java
+++ src/main/java/app/attestation/server/AttestationProtocol.java
@@ -1126,6 +1126,10 @@ class AttestationProtocol {
         }
 
         // key sanity checks
+        if (!teeEnforced.getPurposes().equals(
+                ImmutableSet.of(AuthorizationList.KM_PURPOSE_SIGN, AuthorizationList.KM_PURPOSE_VERIFY))) {
+            throw new GeneralSecurityException("key has invalid purposes");
+        }
         if (teeEnforced.getOrigin() != AuthorizationList.KM_ORIGIN_GENERATED) {
             throw new GeneralSecurityException("not a generated key");
         }
