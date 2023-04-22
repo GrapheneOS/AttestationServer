886f3b87e93a4cb0d6efbdf88f6db132983f13f7
Daniel Micay <danielmicay@gmail.com>
2023-04-22 08:22:16 -0400

    raise minimum patch level to 2019-09-05
    This was the initial patch level for Android 10 on Pixels.

--- src/main/java/app/attestation/server/AttestationProtocol.java
+++ src/main/java/app/attestation/server/AttestationProtocol.java
@@ -178,9 +178,9 @@ class AttestationProtocol {
 
     private static final int AUDITOR_APP_MINIMUM_VERSION = 47;
     private static final int OS_VERSION_MINIMUM = 100000;
-    private static final int OS_PATCH_LEVEL_MINIMUM = 201801;
-    private static final int VENDOR_PATCH_LEVEL_MINIMUM = 201808;
-    private static final int BOOT_PATCH_LEVEL_MINIMUM = 201809;
+    private static final int OS_PATCH_LEVEL_MINIMUM = 201909;
+    private static final int VENDOR_PATCH_LEVEL_MINIMUM = 20190905;
+    private static final int BOOT_PATCH_LEVEL_MINIMUM = 20190905;
 
     private static final String DEVICE_HUAWEI = "Huawei Honor 9 lite / Honor 10 / View 10 / Mate 10 / Mate 20 / Mate 20 lite / P smart 2019 / Pro / P20 / P20 Pro / Y9 2019";
     private static final String DEVICE_HUAWEI_HONOR_7A_PRO = "Huawei Honor 7A Pro / Y7 2019";
