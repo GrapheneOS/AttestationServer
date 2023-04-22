d9f7f3f049543590d867934f5dc95a023463711d
Daniel Micay <danielmicay@gmail.com>
2023-04-22 08:05:30 -0400

    update enforced minimum OS version to Android 10

--- src/main/java/app/attestation/server/AttestationProtocol.java
+++ src/main/java/app/attestation/server/AttestationProtocol.java
@@ -177,7 +177,7 @@ class AttestationProtocol {
     private static final byte AUDITOR_APP_VARIANT_DEBUG = 2;
 
     private static final int AUDITOR_APP_MINIMUM_VERSION = 47;
-    private static final int OS_VERSION_MINIMUM = 80000;
+    private static final int OS_VERSION_MINIMUM = 100000;
     private static final int OS_PATCH_LEVEL_MINIMUM = 201801;
     private static final int VENDOR_PATCH_LEVEL_MINIMUM = 201808;
     private static final int BOOT_PATCH_LEVEL_MINIMUM = 201809;
