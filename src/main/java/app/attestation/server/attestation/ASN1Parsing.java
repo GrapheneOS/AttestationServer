13c0d99b2cc3f015ed33d9ac1fea8c44811a7d99
Brandon Weeks <bweeks@google.com>
2023-06-29 09:02:33 -0700

    upstream-lib: Fix lints/ErrorProne warnings

--- src/main/java/app/attestation/server/attestation/ASN1Parsing.java
+++ src/main/java/app/attestation/server/attestation/ASN1Parsing.java
@@ -23,6 +23,8 @@ import org.bouncycastle.asn1.ASN1Integer;
 /** Utils to get java representation of ASN1 types. */
 class ASN1Parsing {
 
+  private ASN1Parsing() {}
+
   static boolean getBooleanFromAsn1(ASN1Encodable asn1Value) {
     return getBooleanFromAsn1(asn1Value, true);
   }
