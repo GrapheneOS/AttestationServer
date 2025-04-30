b3e1f9de731c296f56d9234d6c91ea5cc17c6c84
Daniel Micay <daniel.micay@grapheneos.org>
2025-04-29 22:31:55 -0400

    switch auto-reboot unit to seconds

--- src/main/java/app/attestation/server/AttestationProtocol.java
+++ src/main/java/app/attestation/server/AttestationProtocol.java
@@ -78,7 +78,7 @@ class AttestationProtocol {
     // byte[] compressedChain { [short encodedCertificateLength, byte[] encodedCertificate] }
     // byte[] fingerprint (length: FINGERPRINT_LENGTH)
     // int osEnforcedFlags
-    // short autoRebootMinutes (-1 for unknown)
+    // int autoRebootSeconds (-1 for unknown)
     // byte portSecurityMode (-1 for unknown)
     // byte userCount (-1 for unknown)
     // byte oemUnlockAllowed (-1 for unknown)
@@ -87,7 +87,7 @@ class AttestationProtocol {
     //
     // Protocol version changes:
     //
-    // 6: autoRebootMinutes added
+    // 6: autoRebootSeconds added
     // 6: portSecurityMode added
     // 6: userCount added
     // 6: oemUnlockAllowed added
@@ -1200,7 +1200,7 @@ class AttestationProtocol {
         }
 
         if (version >= 6) {
-            final short autoRebootMinutes = deserializer.getShort();
+            final int autoRebootSeconds = deserializer.getInt();
             final byte portSecurityMode = deserializer.get();
             final byte userCount = deserializer.get();
             final byte oemUnlockAllowed = deserializer.get();
