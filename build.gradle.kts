9e846f52fe693f1d1a4d6591ec871bb67162caa3
Daniel Micay <daniel.micay@grapheneos.org>
2025-06-05 10:23:55 -0400

    update Bouncy Castle to 1.81

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("com.google.guava:guava:33.4.8-jre")
     implementation("com.google.zxing:core:3.5.3")
     implementation("com.google.zxing:javase:3.5.3")
-    implementation("org.bouncycastle:bcprov-jdk18on:1.80")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.81")
     implementation("org.eclipse.angus:jakarta.mail:2.0.3")
     implementation("org.eclipse.parsson:jakarta.json:1.1.7")
 }
